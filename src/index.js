353df337c98101992bf107a8f6d638fbd5bd24ea
E. M. Greeff <unmodster@gmail.com>
2021-11-16 17:59:10 -0700

    added server, graphql, dtabse

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const { ApolloServer } = require('apollo-server');
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
+
+// 1
+const resolvers = {
+    Query: {
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links,
+    },
+    Mutation: {
+        // 2
+        post: (parent, args) => {
+
+            let idCount = links.length
+
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link)
+            return link
+        }
+    },
+}
+
+const fs = require('fs');
+const path = require('path');
+
+const server = new ApolloServer({
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'),
+        'utf8'
+    ),
+    resolvers,
+})
+
+server
+    .listen()
+    .then(({ url }) =>
+        console.log(`Server is running on ${url}`)
+    );
